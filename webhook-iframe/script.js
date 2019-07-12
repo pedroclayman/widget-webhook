a46c9568945906c565e488835574361034faaec0
Peter Mihalik <peter.mihalik@exponea.com>
2019-07-12 15:17:24 +0200

    add pages

---
+++ webhook-iframe/script.js
@@ -0,0 +1,50 @@
+var appOrigin = '*';
+
+window.parent.postMessage({
+    min_supported_version: 1,
+    max_supported_version: 1,
+    message_type: 'widget_hello'
+  }, appOrigin)
+
+window.addEventListener('message', function(event) {
+  if (event.data.message_type === 'app_hello') {
+
+    if (event.data.widget_state) {
+      if (event.data.widget_state.luckySearch) {
+        luckyRadio.checked = true;
+      } else {
+        normalRadio.checked = true;
+      }
+
+      queryInput.value = event.data.widget_state.query || '';
+    } else {
+        normalRadio.checked = true;
+        queryInput.value = '';
+    }
+
+    window.parent.postMessage({message_type: 'widget_initialized'}, appOrigin)
+  }
+
+  if (event.data.message_type === 'app_request_state') {
+    window.parent.postMessage(
+      {
+        message_type: 'widget_state',
+        webhook: {
+          url: 'https://www.google.com/search?q=' + queryInput.value + (luckyRadio.checked ? '&btnI' : ''),
+          method: 'GET',
+          response_handling: 'text',
+          auth: null,
+          headers: [],
+          body: '',
+          event_properties: {}
+        },
+        widget_state: {
+          luckySearch: luckyRadio.checked,
+          query: queryInput.value
+        }
+      }, appOrigin
+    )
+  }
+
+
+});
